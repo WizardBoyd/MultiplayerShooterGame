8f14e4fbe6fbaf4d568884ee7687ee217a559ec5
WizardBoyd <luis@rivercanalrescue.co.uk>
2023-11-23 09:10:09 +0000

    level wotrk

---
+++ Source/MultiplayerShooter/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+
+// Sets default values
+ALobbyGameMode::ALobbyGameMode()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ALobbyGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALobbyGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	//Get the number of players that have joined
+	int32 NumberOfPlayers = GameState->PlayerArray.Num();
+	if(NumberOfPlayers == 2) //Currently hardcoded for quick testing purposes
+	{
+		if(UWorld* World = GetWorld())
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+		}
+	}
+}
+
---
+++ Source/MultiplayerShooter/GameMode/LobbyGameMode.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+UCLASS()
+class MULTIPLAYERSHOOTER_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ALobbyGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+};
